9bdeb4820bb43a24d73121bbc19b6455ddb2fcd1
StephenieG <39172938+Stephenie2018@users.noreply.github.com>
2026-03-14 01:55:31 +0100

    Remove unused memory include from Core.h
    Removed unnecessary memory header include.

--- src/CryptoNoteCore/Core.h
+++ src/CryptoNoteCore/Core.h
@@ -7,7 +7,6 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
 
-#include <memory"
 #include "P2p/NetNodeCommon.h"
 #include "CryptoNoteProtocol/CryptoNoteProtocolHandlerCommon.h"
 #include "Currency.h"
