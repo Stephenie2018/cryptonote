d0783194ac6f9d299c0c7d5882ed9ed685422c81
StephenieG <39172938+Stephenie2018@users.noreply.github.com>
2026-03-15 01:30:32 +0100

    Update genesis coinbase transaction hex

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "TestCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101b677eb5e6aa96b39aa7a7637c96b40d8d2541f597bd7379dd941b0a160b716a3";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
